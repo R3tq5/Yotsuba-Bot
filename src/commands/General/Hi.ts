41f63cf1c4f2c12fb30d44d1763cd2094f4be460
animeland <102859547+animeland@users.noreply.github.com>
2022-04-05 00:58:34 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://telegra.ph/file/85902fc89a0a6595c7648.mp4";
+			"https://telegra.ph/file/107c5fec65bf16be795d1.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `Hi, lets go on an adventure. use *${this.client.config.prefix}help* if you want anything. \n`,
+				caption: `"Here comes the buffs", super move...... ULTRA CHARGE! use *${this.client.config.prefix}help* if you want anything. \n`,
 			}
 		);
 	};
