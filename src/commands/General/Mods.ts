c74fac9d596209bd1834558036de6bdacfd7de23
animeland <102859547+animeland@users.noreply.github.com>
2022-04-05 01:01:38 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,14 +17,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🏴‍☠️ *Moderators* 🏴‍☠️\n\n'
+        let text = '💖 *Moderators* 💖\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n🏴‍☠️ *Username: ${
+                (text += `#${index + 1}\n💖 *Username: ${
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n🏴‍☠️ luffy 🏴‍☠️ `
+        text += `\n💖 RHOZH FUTABA 💖 `
         return void M.reply(text)
     }
 }
