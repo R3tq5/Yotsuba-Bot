d4b89e018bf19f9d89d92ba200644920d972330e
animeland <102859547+animeland@users.noreply.github.com>
2022-04-14 19:35:54 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "⚡ Here you go ",
-						author: pack[2] || "Yotsuba ⚡",
+						pack: pack[1] || "𝙕𝙀𝙍𝙊❤️",
+						author: pack[2] || "𝘼𝙆𝙐𝙈𝘼",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
