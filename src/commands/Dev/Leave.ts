be151e39f022733dbe5ebdfaa01bd4210f819a4e
R3tq5 <99133787+R3tq5@users.noreply.github.com>
2022-05-30 08:48:22 +0530

    Update Leave.ts

--- src/commands/Dev/Leave.ts
+++ src/commands/Dev/Leave.ts
@@ -10,7 +10,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'leave',
             description: 'Bot Leaves the group',
-            category: 'dev',
+            category: 'general',
             dm: true,
             usage: `${client.config.prefix}leave`,
             modsOnly: true,
